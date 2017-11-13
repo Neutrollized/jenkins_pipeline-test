486ac09c397588e99cad790724d188bb5f6706e3
Glen Yu <Glen.Yu@Cantire.com>
2017-11-13 15:18:55 -0500

    added try/catch

--- jenkins_pipeline.groovy
+++ jenkins_pipeline.groovy
@@ -1,9 +1,19 @@
 node {
-    stage ('Stage 1') {
-        echo 'Hello World 1'
+    try {
+        wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
+            echo "\u27A1 inside the try"
+    
+            stage ('Stage 1') {
+                echo 'Hello World 1'
+            }
+    
+            stage ('Stage 2') {
+                echo 'Hello World 2'
+            }
+        }
+    }
+    catch (err) {
+        echo "\u27A1 Caught: ${err}"
     }
     
-    stage ('Stage 2') {
-        echo 'Hello World 2'
-    }
 }
