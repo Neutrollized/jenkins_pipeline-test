f7afe8b0e448b94c4ba38cf2509d3a5fb7fae09f
Glen Yu <Glen.Yu@Cantire.com>
2017-11-14 09:08:26 -0500

    added stuff

--- jenkins_pipeline.groovy
+++ jenkins_pipeline.groovy
@@ -1,19 +1,64 @@
 node {
     try {
-        wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
-            echo "\u27A1 inside the try"
-    
-            stage ('Stage 1') {
-                echo 'Hello World 1'
+        // requires AnsiColor plugin
+        wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
+            echo "\u27A1 Begins here..."
+            stage ('Clean workspace') {
+                deleteDir()
+                sh 'pwd'
+                sh 'ls -lah'
             }
-    
-            stage ('Stage 2') {
-                echo 'Hello World 2'
+
+            stage ('Checkout source') {
+                // Pull the latest commit of the specified branch(es)
+                // of the specified submodule(s)
+                checkout scm
+                sh 'git submodule update --remote --init'
+            }  
+        }
+
+        stage ('Build') {
+            echo 'Compiling software'
+        }
+
+        // scripted parallel
+        stage ('Parallel stage') {
+            parallel "integration test 1": {
+                echo 'Running integration test 1'
+            },
+            "integration test 2": {
+                echo 'Running integration test 2'
+            },
+            "ui tests": {
+                echo 'Running UI functional tests'
             }
         }
+
+/* 
+        // declarative parallel?
+        stage ('Parallel stage') {
+            parallel {
+                stage ('Run integration test 1') {
+                    steps {
+                        echo 'Running test 1'
+                    }
+                }
+                stage ('Run integration test 2') {
+                    steps {
+                        echo 'Running test 2'
+                    }
+                }
+                stage ('Run UI functional tests') {
+                    steps {
+                        echo 'Running test 3'
+                    }
+                }
+            }
+        }
+*/
     }
     catch (err) {
         echo "\u27A1 Caught: ${err}"
     }
-    
+
 }
