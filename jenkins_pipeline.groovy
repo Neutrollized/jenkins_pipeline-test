c2ce1a7b63ec8af7dde2bc6214e0aea640615e96
Neutrollized <glen.yu@gmail.com>
2017-11-17 10:42:54 -0500

    typo

--- jenkins_pipeline.groovy
+++ jenkins_pipeline.groovy
@@ -28,7 +28,7 @@ node('docker') {
             }
         }
 
-	docker.image('neutrollized/chromium-xvbf-ng:1.1.0').inside('--net=host') {
+	docker.image('neutrollized/chromium-xvfb-ng:1.1.0').inside('--net=host') {
 	    stage ('Compiling project within docker container') {
 	    	sh 'cd test-code/angular-realworld-example-app && npm install && ng build'
 	    }
