5afc1951e2f28c7a7a031b615691043db6c51522
Neutrollized <glen.yu@gmail.com>
2017-11-17 00:58:21 -0500

    test

--- jenkins_pipeline.groovy
+++ jenkins_pipeline.groovy
@@ -33,7 +33,7 @@ node('docker') {
 	    	sh 'cd test-code/angular-realworld-example-app && npm install && ng build'
 	    }
 	    stage ('Start ng serve') {
-		sh "cd test-code/angular-realworld-example-app && ng serve --host `hostname`"
+		sh "cd test-code/angular-realworld-example-app && ng serve --host 0.0.0.0"
             }
             stage ('Parallel testing within docker container') {
             	parallel "docker test 1": {
