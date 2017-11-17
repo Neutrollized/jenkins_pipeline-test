b5e69ff0f5219685296f821ee02d95a54f290189
Neutrollized <glen.yu@gmail.com>
2017-11-17 16:03:19 -0500

    testing alpine image

--- jenkins_pipeline.groovy
+++ jenkins_pipeline.groovy
@@ -28,16 +28,17 @@ node('docker') {
             }
         }
 
-	docker.image('neutrollized/chromium-headless-ng:1.1.0').inside('--privileged -p 4200:4200 -p 9876:9876 -p 49152:49152') {
+	//docker.image('neutrollized/chromium-headless-ng:1.1.0').inside('--privileged -p 4200:4200 -p 9876:9876 -p 49152:49152') {
+	docker.image('neutrollized/chromium-xvfb-ng:1.1.0').inside('--privileged -p 4200:4200 -p 9876:9876 -p 49152:49152') {
 	    stage ('Compiling project within docker container') {
 	    	sh 'cd test-code/angular-realworld-example-app && npm install && ng build'
+		sh 'xvfb :99 -ac &'
+		sh 'export DISPLAY=:99.0'
 	    }
-/*
 	    stage ('Unit test') {
 		sh 'npm install karma'
 		sh 'cd test-code/angular-realworld-example-app && ng test --watch=false --browsers ChromeHeadless'
             }
-*/
 	    stage ('Start ng serve') {
 		sh 'cd test-code/angular-realworld-example-app && ng serve --host 0.0.0.0 --disable-host-check &'
 		sh 'sleep 30'
