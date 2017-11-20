5ba3860743a33248cea34fa51381e13063a25693
Neutrollized <glen.yu@gmail.com>
2017-11-20 10:52:28 -0500

    added variables

--- jenkins_pipeline.groovy
+++ jenkins_pipeline.groovy
@@ -1,7 +1,7 @@
 //@Library('github.com/Neutrollized/shared-jenkins-library@master')
 
 // docker vars
-if (!env.DOCKER_NODEJS) {env.DOCKER_NODEJS = 'neutrollized/chromium-headless-nodejs'}
+if (!env.DOCKER_NODEJS) {env.DOCKER_NODEJS = 'neutrollized/headless-chrome-nodejs'}
 if (!env.NODEJS_VER) {env.NODEJS_VER = '6.11.2'}
 if (!env.KARMA_PORT) {env.KARMA_PORT = '-p 9876:9876'}
 if (!env.PROTRACTOR_PORT) {env.PROTRACTOR_PORT = '-p 49152:49152'}
@@ -15,7 +15,8 @@ if (!env.TEST_OPTS) {env.TEST_OPTS = '--progress=false'}
 if (!env.E2E_OPTS) {env.E2E_OPTS = '--progress=false'}
 
 // Mandatory Jenkinsfile vars
-//if (!env.BUILD_REPO) {error 'BUILD_REPO must be defined in Jenkinsfile environment.'}
+if (!env.PROJECT_REPO) {error 'PROJECT_REPO must be defined in Jenkinsfile environment.'}
+if (!env.PROJECT_DIR) {error 'PROJECT_DIR must be defined in Jenkinsfile environment.'}
 
 node('docker') {
     try {
@@ -37,13 +38,13 @@ node('docker') {
 
         stage ('Compile C') {
             wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
-                sh 'gcc -Wall test-code/hello.c -o test-code/hello'
+                sh "gcc -Wall ${env.PROJECT_REPO}/hello.c -o ${env.PROJECT_REPO}/hello"
             }
         }
 
         stage ('Compile Go') {
             wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
-                sh 'go build -o ./test-code/hello-world ./test-code/hello-world.go'
+                sh "go build -o ${env.PROJECT_REPO}/hello-world ${env.PROJECT_REPO}/hello-world.go"
             }
         }
 
@@ -54,7 +55,7 @@ node('docker') {
 		// ng gets installed under node_modules/, so path to ng binary need to be explicit
 		// github.com/angular/angular-cli/issues/503
 	    	sh """
-		    cd test-code/angular-realworld-example-app
+		    cd ${env.PROJECT_REPO}/${env.PROJECT_DIR}
                     npm install ${env.NPM_OPTS}
 		    ${env.NG_PATH}/ng --version
 		    ${env.NG_PATH}/ng build ${env.BUILD_OPTS}
@@ -63,7 +64,7 @@ node('docker') {
 	    stage ('Karma Unit test') {
 		sh "npm install ${env.NPM_OPTS} karma"
 		sh """
-		    cd test-code/angular-realworld-example-app
+		    cd ${env.PROJECT_REPO}/${env.PROJECT_DIR}
 		    ${env.NG_PATH}/ng test --watch=false ${env.TEST_OPTS} --browsers ChromeHeadless
 		"""
             }
@@ -75,7 +76,7 @@ node('docker') {
             stage ('Parallel testing within docker container') {
             	parallel "Protractor E2E test": {
                     sh """
-			cd test-code/angular-realworld-example-app
+		        cd ${env.PROJECT_REPO}/${env.PROJECT_DIR}
 			npm install ${env.NPM_OPTS} protractor
 			${env.NG_PATH}/ng e2e ${env.E2E_OPTS}
 		    """
@@ -92,10 +93,10 @@ node('docker') {
         // scripted parallel
         stage ('Parallel testing stage') {
             parallel "integration test 1": {
-                sh 'test-code/hello'
+                sh "${env.PROJECT_REPO}/hello"
             },
             "integration test 2": {
-                sh 'test-code/hello-world'
+                sh "${env.PROJECT_REPO}/hello-world"
             },
             "ui tests": {
 		sh 'echo stuff here'
